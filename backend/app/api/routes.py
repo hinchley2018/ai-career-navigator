1088a0ebf4bda3c7078f2298804a8bcbcfc72da6
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2024-09-14 09:53:18 -0500

    use fastapi and backend from tutorial to build websockets

--- backend/app/api/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify
-from app.extensions import mongo
-
-api_blueprint = Blueprint('api', __name__)
-
-@api_blueprint.route('/items', methods=['GET'])
-def get_items():
-    items = mongo.db.items.find()
-    return jsonify([item for item in items])
